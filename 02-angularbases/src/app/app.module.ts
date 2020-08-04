a7c19c4f23315c11304920979e6cdcaecbc056e1
JabGT <69199533+JabGT@users.noreply.github.com>
2020-08-04 14:07:43 +0200

    Load children issue
    Cunado compilo este codigo no me encuentra la ruta de load children

---
+++ 02-angularbases/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    //AboutComponent,
+    //ContactComponent,
+    //HomeComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
